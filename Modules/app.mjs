71473584e1b78e973d847f23694db159185aa002
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-09-18 09:45:55 +0530

    Using modules with mjs extension

---
+++ Modules/app.mjs
@@ -0,0 +1,8 @@
+// app.js
+
+// Importing specific items from the module
+import { add, subtract, PI } from './mathModule.mjs';
+
+console.log(add(10, 5));       // Output: 15
+console.log(subtract(10, 5));  // Output: 5
+console.log(PI);               // Output: 3.14159
